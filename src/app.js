9ab20f6ce6795adf270cb0e54690ea8b378ea085
bq574lk3r <bq.574lk3r@gmail.com>
2024-01-24 16:41:11 +0300

    add swagger custom css

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const app = express();
 const Sentry = require("@sentry/node");
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('../swaggerSpec.js'); 
+const path = require('path')
 
 require('dotenv').config();
 const PORT = process.env.PORT;
@@ -21,9 +22,12 @@ app.use(Sentry.Handlers.tracingHandler());
 
 app.use(express.json());
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, { customCssUrl: '/swaggerCustoms/style/docs.css' }));
 
 app.use(router);
+
+app.use(express.static(path.join(__dirname, '..', 'static')));
+
 
 app.use(Sentry.Handlers.errorHandler());
 
